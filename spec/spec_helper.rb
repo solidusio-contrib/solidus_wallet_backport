fd4add5baec3ee75324c017997ecc27295ad0790
Alessandro Lepore <lepore.ale@gmail.com>
2017-05-11 15:05:45 +0200

    Add Wallet and WalletPaymentSource specs
    Copied from Solidus 2.2

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,7 @@ require File.expand_path('../dummy/config/environment.rb', __FILE__)
 require 'rspec/rails'
 require 'database_cleaner'
 require 'ffaker'
+require 'with_model'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -35,6 +36,7 @@ require 'solidus_wallet_backport/factories'
 
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
+  config.extend WithModel
 
   # Infer an example group's spec type from the file location.
   config.infer_spec_type_from_file_location!
